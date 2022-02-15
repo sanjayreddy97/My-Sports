e9b58530b3144f948d45a4a4f26968d89494471a
sanjayreddy <sanjayreddy@sanjays-air.attlocal.net>
2022-02-14 23:56:22 -0600

    Lab 4 - Initial Commit

---
+++ src/main/java/edu/iit/sat/itmd4515/smuthyala/config/Driver.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.iit.sat.itmd4515.smuthyala.config;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author sanjayreddy
+ */
+public class Driver {
+    
+    public static void main(String ... args){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("itmd4515testPU");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        //
+        tx.commit();
+    }
+    
+}
