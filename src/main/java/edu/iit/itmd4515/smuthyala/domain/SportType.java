40c9f102c6d579efb85cd78b65e02bbcab4d844e
sanjayreddy <sanjayreddy@sanjays-air.attlocal.net>
2022-03-30 15:51:48 -0500

    Lab 7 - Modified classes and view components

--- src/main/java/edu/iit/itmd4515/smuthyala/domain/SportType.java
+++ src/main/java/edu/iit/itmd4515/smuthyala/domain/SportType.java
@@ -9,5 +9,21 @@ package edu.iit.itmd4515.smuthyala.domain;
  * @author sanjayreddy
  */
 public enum SportType {
-    BASEBALL,SOCCER,HANDBALL,BASKETBALL,CRICKET,RUGBY;
+    BASEBALL("Baseball"),
+    SOCCER("Soccer"),
+    HANDBALL("Handball"),
+    BASKETBALL("Basketball"),
+    CRICKET("Cricket"),
+    RUGBY("Rugby");
+    
+    private String label;
+
+    private SportType(String label){
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+    
 }
